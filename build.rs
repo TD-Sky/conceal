90881e924032dbe1f9f0d998bab1ef5b3c645e5c
TD-Sky <three-dim-sky@foxmail.com>
2022-11-10 16:23:00 +0800

    try to use zsh-complete

--- build.rs
+++ build.rs
@@ -5,7 +5,7 @@ use self::cli::Cli;
 use clap::CommandFactory;
 use clap_complete::{
     generate_to,
-    Shell::{Bash, Fish},
+    Shell::{Bash, Fish, Zsh},
 };
 use clap_complete_nushell::Nushell;
 use std::io::Result;
@@ -15,8 +15,7 @@ fn main() -> Result<()> {
     let name = "cnc";
     let dir = "completions";
 
-    // Don't use clap's support for zsh.
-    // TODO: zsh completion should be written manually
+    generate_to(Zsh, cmd, name, dir)?;
     generate_to(Bash, cmd, name, dir)?;
     generate_to(Fish, cmd, name, dir)?;
     generate_to(Nushell, cmd, name, dir)?;
