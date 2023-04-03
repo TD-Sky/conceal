9f7af42cd631eba472cd615a9a3c6ba9e1c4cb7d
TD-Sky <three-dim-sky@foxmail.com>
2023-04-03 08:47:21 +0800

    feat(completions): ignore the completions

--- build.rs
+++ build.rs
@@ -8,6 +8,7 @@ use clap::{Command, CommandFactory};
 use clap_complete::generate_to;
 use clap_complete::Shell::{Bash, Fish, Zsh};
 use clap_complete_nushell::Nushell;
+use std::fs;
 use std::io;
 use std::path::PathBuf;
 
@@ -20,6 +21,7 @@ fn main() -> io::Result<()> {
 
 fn generate_completions(cmd: &mut Command, name: &'static str) -> io::Result<()> {
     let dir = &PathBuf::from("completions").join(name);
+    fs::create_dir_all(dir)?;
 
     generate_to(Zsh, cmd, name, dir)?;
     generate_to(Bash, cmd, name, dir)?;
