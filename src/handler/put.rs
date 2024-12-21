11ad0ff97d2b882b470e2918e3f8db5cb4efde51
cncsmonster <cncsmonster@163.com>
2024-12-22 00:13:41 +0800

    fix: update trash dep to 5.2.1, and fix delete function in macos platform

--- src/handler/put.rs
+++ src/handler/put.rs
@@ -1,7 +1,5 @@
 use std::path::Path;
 
-use trash::delete_all;
-
 use crate::error::Result;
 
 pub fn put(items: &[impl AsRef<Path>]) -> Result<()> {
@@ -11,7 +9,20 @@ pub fn put(items: &[impl AsRef<Path>]) -> Result<()> {
         return Err("Please specify the files to trash".into());
     }
 
-    delete_all(items)?;
+    #[cfg(target_os = "macos")]
+    {
+        use trash::{
+            macos::{DeleteMethod, TrashContextExtMacos},
+            TrashContext,
+        };
+        let mut ctx = TrashContext::default();
+        ctx.set_delete_method(DeleteMethod::NsFileManager);
+        ctx.delete_all(items)?;
+    }
+    #[cfg(all(not(target_os = "macos"), not(target_os = "android")))]
+    {
+        trash::delete_all(items)?;
+    }
 
     Ok(())
 }
